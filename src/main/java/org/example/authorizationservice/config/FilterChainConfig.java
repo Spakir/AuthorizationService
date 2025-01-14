aaf50462832ae5402fe9eae4791e97355dba7d6f
Spakir <danntyl@mail.ru>
2025-01-14 10:23:03 +0300

    jwt-auth

--- src/main/java/org/example/authorizationservice/config/FilterChainConfig.java
+++ src/main/java/org/example/authorizationservice/config/FilterChainConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
